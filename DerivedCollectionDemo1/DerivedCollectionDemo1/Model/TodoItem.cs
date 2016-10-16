af28ac73d46e1915ba3369ec3b6a41e85be7defc
Jan Hannemann <jan.hannemann@outlook.com>
2016-10-16 13:59:44 -0700

    DerivedCollection Demo 1

---
+++ DerivedCollectionDemo1/DerivedCollectionDemo1/Model/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DerivedCollectionDemo1.Model
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool? Done { get; set; }
+    }
+}
---
+++ DerivedCollectionDemo1/DerivedCollectionDemo1/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using DerivedCollectionDemo1.Model;
+using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+
+namespace DerivedCollectionDemo1.ViewModel
+{
+    public class MainViewModel : ReactiveObject
+    {
+        private readonly DataSerivce _dataService;
+        private ReactiveList<TodoItem> _rootList;
+        private IReactiveDerivedList<TodoItem> _items;
+        private int _count;
+
+
+        public IReactiveDerivedList<TodoItem> Items
+        {
+            get { return _items; }
+            set { this.RaiseAndSetIfChanged(ref _items, value); }
+        }
+
+
+        public int Count
+        {
+            get { return _count; }
+            set { this.RaiseAndSetIfChanged(ref _count, value); }
+        }
+
+
+        public MainViewModel()
+        {
+            _dataService = new DataSerivce();
+            _rootList = new ReactiveList<TodoItem>();
+
+            Items = _rootList.CreateDerivedCollection(x => x, x => true, (x, y) => x.DueDate.CompareTo(y.DueDate));
+
+            // Subscribe to the count changed observable so we can update our Count property.
+            Items.CountChanged.Subscribe(x => Count = x);
+
+
+            _dataService.Listen()
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(x =>
+                {
+                    _rootList.Add(x);
+                });
+
+
+            Load(10);
+        }
+
+        public void Load(int desiredNumber)
+        {
+            _dataService.Load(desiredNumber);
+        }
+    }
+}
